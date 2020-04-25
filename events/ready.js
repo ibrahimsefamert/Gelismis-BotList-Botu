61d70c871c3bd9a8671bdcfdcee6b39c7119da80
Glitch (narrow-glossy-) <none>
2020-04-25 09:21:25 +0000

    🚵💨 Checkpoint ./ayarlar.json:2652653/202 ./events/ready.js:2652653/33

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ const ayarlar = require("../ayarlar.json");
 var prefix = ayarlar.prefix;
 
 module.exports = async client => {
-  var oyun = ["youtube.com/Lord"];
+  var oyun = ["LYNX İNVİTE"];
 
   setInterval(async () => {
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);

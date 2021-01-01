55302691d5f7b8b7318b42f49d144c6eb0d91c1f
Glitch (narrow-glossy-) <none>
2021-01-01 13:34:36 +0000

    🎱🎁 Checkpoint ./komutlar/ailemiz.js:13225190/964

--- komutlar/ailemiz.js
+++ komutlar/ailemiz.js
@@ -6,10 +6,11 @@ exports.run = (bot, message) => {
     const guilds = guildArray.splice(0,25);
     for (const guild of guilds) {
       embed.addField(`**${guild.name}** - ÜYE SAYISI : **${guild.memberCount}**`);
+      embed.addField(`Sunucu ID:  **${guild.id}**`)
       embed.setColor('BLACK')
       embed.setTitle('Ailemiz;')
-      embed.setDescription(`Büyük bir ailedeyiz ! Ailemde **${bot.guilds.size}** kadar sunucu var !`)
-      embed.setFooter(`**${guild.name}**, ${guild.avatarURL}`)
+      embed.setDescription(`Büyük bir ailedeyiz !. Ailemde **${bot.guilds.size}** kadar sunucu var !`)
+      embed.setFooter(bot.user.username, bot.user.avatarURL);
     }
     message.channel.send({embed: embed});
   }
@@ -17,8 +18,8 @@ exports.run = (bot, message) => {
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: ['goster', 'g'],
+  guildOnly: true,
+  aliases: ['ailemiz'],
   permLevel: 4
 };
 

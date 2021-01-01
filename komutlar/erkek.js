0f554d5e5f47ac1b234ec9209d59eb6c69801590
Glitch (narrow-glossy-) <none>
2021-01-01 18:04:21 +0000

    🍚🐕 Checkpoint ./komutlar/erkek.js:13225190/1686 ./komutlar/kadın.js:13225190/1920

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -54,12 +54,12 @@ message.guild.members.get(kullanıcı.id).send(emb.setDescription(`• Kaydın b
 let embed2 = new Discord.RichEmbed()
 .setTitle(`• Bir Kullanıcı Kayıt Oldu.`)
 .setDescription(`
-• **Kayıt Olan Kullanıcı:** ${kullanıcı} \`
+• **Kayıt Olan Kullanıcı:** ${kullanıcı}
 • **İsim Yaş:** \` ${isim} | ${yaş} \`
 • **Verilen Rol:** <@&${erkekROL}> \`
 • **Bu Hesap:** \`  { ${kontrol} }  \`
 • **Sunucumuz şu an** \` ${message.guild.members.size} \`** kişi **
-• **Kayıt eden:** ${message.author} \`  
+• **Kayıt eden:** ${message.author}   
 `)
 .setImage('https://i.pinimg.com/originals/af/80/39/af8039261a387be71514bb4c2e5e54b5.gif')
 
@@ -69,12 +69,12 @@ client.channels.get(ayarlar.kayıtLOG).send(embed2)
 let embed3 = new Discord.RichEmbed()
 .setTitle(`• Kayıt Başarıyla Tamamlandı!.`)
 .setDescription(`
-• **Kayıt Olan Kullanıcı:** ${kullanıcı} \`
+• **Kayıt Olan Kullanıcı:** ${kullanıcı}
 • **İsim Yaş:** \` ${isim} | ${yaş} \`
-• **Verilen Rol:** <@&${erkekROL}> \`
+• **Verilen Rol:** \` <@&${erkekROL}> \`
 • **Bu Hesap:** \`  { ${kontrol} }  \`
 • **Sunucumuz şu an** \` ${message.guild.members.size} \`** kişi **
-• **Kayıt eden:** ${message.author} \`  
+• **Kayıt eden:** ${message.author}    
 `)
 .setImage('https://i.pinimg.com/originals/af/80/39/af8039261a387be71514bb4c2e5e54b5.gif')
 message.channel.send(embed3)

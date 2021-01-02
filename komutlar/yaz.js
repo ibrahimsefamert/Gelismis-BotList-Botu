dc5c15d4af66f552251de54860ed4db9e057d5b5
Glitch (narrow-glossy-) <none>
2021-01-02 18:44:50 +0000

    🐝💒 Checkpoint ./komutlar/yaz.js:15570898/239 ./komutlar/kod.js:15570898/115

--- komutlar/yaz.js
+++ komutlar/yaz.js
@@ -2,30 +2,30 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
     let kod = args.slice(0).join(' ');
-    if (kod.length < 1) return message.reply('<a:unlem:794638042484441170>**Yanlış kullanım** \n <a:gul:794639948409339914> **Doğru Kullanım :** ``+kod <kodunuz>``');
+    if (kod.length < 1) return message.reply('<a:unlem:794638042484441170>**Yanlış kullanım** \n <a:gul:794639948409339914> **Doğru Kullanım :** ``+yaz <yazınız>``');
     let kodadı = args.slice(0).join(' ');
    if (kodadı.length < 1) return message.reply('Lütfen Kodun Adını Giriniz');
    let kodaçk = args.slice(0).join(' ');
   if (kodadı.length < 1) return message.reply('Lütfen Kodun Açıklamasını Giriniz');
     message.delete();
     const embed = new Discord.RichEmbed()
-    .setColor('RANDOM')
-     .setTitle('**<a:tr:794638011320107008>  Atlantic Code ™ İyi Kodlamalar Diler | Developed By Sefa MERT  <a:tr:794638011320107008>**')
-    .setDescription(`\`\`\`${kod}\`\`\``)
+    .setColor('RED')
+     .setTitle('**<a:kirmizitac:794638228782055474>  Atlantic Code & BotList™ | Developed By Sefa MERT  <a:kirmizitac:794638228782055474>**')
+    .setDescription(`${kod}`)
     .setTimestamp()
-    .setFooter(`Kod, ${message.author.username} Tarafından Eklendi`, message.author.avatarURL)
+    .setFooter(`${message.author.username} Tarafından İstendi`, message.author.avatarURL)
     
     return message.channel.sendEmbed(embed);
 }; 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['k', 'kodpaylaş'],
+  aliases: ['y', 'yazı'],
   permLevel: 0
 };
 
 exports.help = {
-  name: "kod",
+  name: "yaz",
   description: "Bot yazı yazar.",
   usage: "yaz"
 }; 

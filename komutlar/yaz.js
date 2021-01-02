6826596433f66a0b6ccfe6bc11177ad7771f658a
Glitch (narrow-glossy-) <none>
2021-01-02 13:22:09 +0000

    🐈💧 Checkpoint ./komutlar/yaz.js:15570898/539

--- komutlar/yaz.js
+++ komutlar/yaz.js
@@ -2,10 +2,14 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
     let kod = args.slice(0).join(' ');
-    if (kod.length < 1) return message.reply('L');
+    if (kod.length < 1) return message.reply('Yanlış komut! ');
+    let kodadı = args.slice(0).join(' ');
+   if (kodadı.length < 1) return message.reply('Lütfen Kodun Adını Giriniz');
+   let kodaçk = args.slice(0).join(' ');
+  if (kodadı.length < 1) return message.reply('Lütfen Kodun Açıklamasını Giriniz');
     message.delete();
     const embed = new Discord.RichEmbed()
-    .addField("ASD", "ASASD")
+    .addField("${kodadı}", "${kodaçk}")
     .setColor('RANDOM')
      .setTitle('**<a:tr:794638011320107008> Atlantic Code ™ İyi Kodlamalar Diler | Developed By Sefa MERT <a:tr:794638011320107008>**')
     .setDescription(`\`\`\`${kod}\`\`\``)

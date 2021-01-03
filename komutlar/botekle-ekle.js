631d00519741465c2920133869aa7d10ed082383
Glitch (narrow-glossy-) <none>
2021-01-03 00:55:29 +0000

    🌑🙉 Checkpoint ./komutlar/botekle-ekle.js:15570898/28818

--- komutlar/botekle-ekle.js
+++ komutlar/botekle-ekle.js
@@ -9,20 +9,21 @@ exports.run = function(client, message, args) {
 	let kanal = "795015414379839488" // başvurunun yapılacağı kanal
   let log = "795015559921008650" // bot eklendi / onaylandı / reddedildi kanalı
 	
-  if (message.channel.id !== kanal) return message.channel.send(`<a:unlem:794638042484441170> **Botunuz için sadece <#${kanal}> kanalında başvuruda bulunabilirsin.**`).then(msg => msg.delete(10000))
+  if (message.channel.id !== kanal) return message.channel.send(`<a:unlem:794638042484441170> **Botunuz için sadece <#${kanal}> kanalında başvuruda bulunabilirsiniz.**`).then(msg => msg.delete(10000))
 	if (message.channel.id == kanal) {
   if (!botid) return message.channel.send(`<a:unlem:794638042484441170>  **Botunun idsini yazmalısın.** \n **Örnek :** \`\`!botekle <bot-id> <bot-prefix>\`\``).then(msg => msg.delete(10000))
-  if (!prefix) return message.channel.send(`<a:unlem:794638042484441170>  **Botunun prefixini yazmalısın.**\n **Örnek :** \`\`!botekle <bot-id> <bot-prefix>\`\``).then(msg => msg.delete(10000))
+  if (!prefix) return message.channel.send(`<a:unlem:794638042484441170> **Botunun prefixini yazmalısın.**`).then(msg => msg.delete(10000))
+  
   message.delete()
   const embed = new Discord.RichEmbed()
   .setColor("ff0000")
   .setDescription(`[Botu Test İçin Ekle](https://discordapp.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=0)`, true)
   .setTitle("<a:kirmizitac:794638228782055474>  Atlantic BotList ™  <a:kirmizitac:794638228782055474>")
-  .addField("Bot Sahip Adı", message.author.tag )
-  .aDdField("Bot Sahip İD", message.author.id)
-  .addField("Botun İDsi", botid)
-  .addField("Prefix", prefix)
-  
+  .addField("Bot Sahibi", message.author.tag )
+  .addField("Bot Sahibi İd", message.author.id)
+  .addField("Bot ID", botid)
+  .addField("Bot Prefix",prefix)
+
   client.channels.get(basvuru).send(embed)
   client.channels.get(log).send(`<:bekliyor:794990233036783657>  ${message.author} **adlı geliştiricinin; <@${botid}> adlı, __${botid}__ idli botu inceleme sırasına alındı.**`)
   message.channel.send(`<a:onay:794638422492315680> **${message.author}, botunuz başarıyla inceleme sırasına alınmıştır. Bizimle çalıştığınız için teşekkürler.**`).then(msg => msg.delete(10000))
@@ -32,7 +33,7 @@ exports.run = function(client, message, args) {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['bot-ekle','ekle','bot'],
+  aliases: ['bot-ekle','ekle'],
   permLevel: 0
 };
 

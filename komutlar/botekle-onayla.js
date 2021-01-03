b586eab4c20ba254e234200f8383a43933ba393e
Glitch (narrow-glossy-) <none>
2021-01-03 09:03:23 +0000

    🎢💨 Checkpoint ./komutlar/botekle-onayla.js:15570898/1593

--- komutlar/botekle-onayla.js
+++ komutlar/botekle-onayla.js
@@ -18,18 +18,18 @@ exports.run = function(client, message, args) {
   message.delete()
   const embed = new Discord.RichEmbed()
   .setColor("ff0000")
-  .setDescription(`<a:tik:794989992296054856> Botu Sunucuna Ekle : [8 Perm](https://discordapp.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=8) & [0 Perm](https://discordapp.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=0)`, true)
-  .setTitle("<a:kirmizitac:794638228782055474>  Atlantic Code & BotList ™ | Dev. By Sefa MERT<a:kirmizitac:794638228782055474>")
-  .addField("<a:parlayandeveloper:794989633385660426> Botun Geliştiricisi", sa)
-  .addField("<a:onayy:795014859066179615> Botun İdsi", botid)
+  .setTitle("<a:kirmizitac:794638228782055474>  Atlantic Code & BotList ™ | Dev. By Sefa MERT <a:kirmizitac:794638228782055474>")
+  .addField("<a:tik:794989992296054856> Botu Sunucuna Ekle", "[0 Perm Ekle](https://discordapp.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=0) & [8 Perm Ekle](https://discordapp.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=8)")
+  .addField("<a:parlayandeveloper:794989633385660426> Botun Geliştiricisinin İdsi", sahip)
+  .addField("<a:ucgenler:794989953934688296> Botun İdsi", botid)
   .addField("<a:donenelmas:794639977077145602> Botun Prefixi", prefix)
+  .addField("<a:down:794990602277093406> Botu Onaylayan Yetkili", message.author.tag )
   .setTimestamp()
-  .setFooter("Bu bot, Sefa MERT tarafından geliştirlmiştir","https://media.discordapp.net/attachments/794721957677367356/795212264214233098/20200806_002234.jpg?width=473&height=473")
+  .setFooter("Bu bot, Sefa MERT tarafından geliştirlmiştir.","https://media.discordapp.net/attachments/794721957677367356/795212264214233098/20200806_002234.jpg?width=473&height=473")
   client.channels.get(botsahipleri).send(embed)
 
-	client.channels.get(log).send(`<:tsl_onaylandi:769298286623981590> <@${sahip}> **adlı geliştiricinin; <@${botid}> adlı, __${botid}__ idli botu onaylandı.**\n __*Botu Onaylayan Yetkili :*__ ${message.author} - **${message.author.tag}**`);
-		client.channels.get(botsahipleri).send(`<a:kirmizitac:794638228782055474>  **Atlantic BotList ™** <a:kirmizitac:794638228782055474> \n <a:down:794990602277093406> **Botun Adı :** <@${botid}> \n <a:parlayandeveloper:794989633385660426> **Botun Geliştiricisi :** <@${sahip}> \n __*Botu Onaylayan Yetkili :*__ ${message.author} - **${message.author.tag}**`)
-  message.channel.send(`<a:onay:794638422492315680> **Başarıyla Botu Onayladınız.**`).then(msg => msg.delete(10000))
+	client.channels.get(log).send(`<:tsl_onaylandi:769298286623981590> <@${sahip}> **adlı geliştiricinin; <@${botid}> adlı, __${botid}__ idli botu onaylandı.**\n __*Botu Onaylayan Yetkili :*__ ${message.author} - **${message.author.tag}**`)
+  message.channel.send(`<a:onay:794638422492315680> **Başarıyla <@{botid}> adlı botu onayladınız.**`).then(msg => msg.delete(10000))
 };//Atlantic Code'ye Aittir.
 
 exports.conf = {

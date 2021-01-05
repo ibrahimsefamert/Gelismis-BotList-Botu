b46ab10f30a1a68eda68ef32b752bc425f4c2372
Glitch (narrow-glossy-) <none>
2021-01-05 20:09:09 +0000

    🚀🛍 Checkpoint ./komutlar/afk.js:15570898/1719 ./server.js:15570898/2416

--- server.js
+++ server.js
@@ -205,6 +205,35 @@ client.on('message', msg => {
   }
 );
 
+//AFK SİSTEMİ
+client.on("message" , async msg => {
+  if(msg.content.startsWith(ayarlar.prefix+"afk")) return;
+ 
+  let afk = msg.mentions.users.first()
+ 
+  const kisi = db.fetch(`afkid_${msg.author.id}_${msg.guild.id}`)
+ 
+  const isim = db.fetch(`afkAd_${msg.author.id}_${msg.guild.id}`)
+ if(afk){
+   const sebep = db.fetch(`afkSebep_${afk.id}_${msg.guild.id}`)
+   const kisi3 = db.fetch(`afkid_${afk.id}_${msg.guild.id}`)
+   if(msg.content.includes(kisi3)){
+ 
+       msg.reply(`Etiketlediğiniz Kişi Afk \n Sebep : ${sebep}`)
+   }
+ }
+  if(msg.author.id === kisi){
+ 
+       msg.reply(`Afk'lıktan Çıktınız`)
+  db.delete(`afkSebep_${msg.author.id}_${msg.guild.id}`)
+  db.delete(`afkid_${msg.author.id}_${msg.guild.id}`)
+  db.delete(`afkAd_${msg.author.id}_${msg.guild.id}`)
+   msg.member.setNickname(isim)
+   
+ }
+ 
+});
+
 
 //DASHBOARD
 client.on('message', msg => {

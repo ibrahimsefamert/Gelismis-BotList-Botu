2e20e8fea3cd64d5b28f5c083e8954d7ba8310cb
Glitch (narrow-glossy-) <none>
2021-01-07 18:51:17 +0000

    🌲💕 Checkpoint ./events/ready.js:27082998/2127 ./server.js:27082998/7849 ./LICENSE.md:27082998/1072 ./ayarlar.json:27082998/381

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,30 @@
-const chalk = require("chalk");
-const moment = require("moment");
-const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json");
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
 var prefix = ayarlar.prefix;
 
-module.exports = async client => {
-  var oyun = ["Sefa MERT"];
+module.exports = client => {
+var oyun = [
+        "SefaMERT ♥",
+        "youtube.com/SefaMERT"
+     
+    ];
 
-  setInterval(async () => {
-    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
-  }, 12000);
-  client.user.setStatus("dnd");
+    setInterval(function() {
 
-  /*client.user.setActivity("a!yardım | a!davet | a!prefix ☣", { type: "WATCHING" });*/
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], "youtube.com/SefaMERT" );
+      }, 2 * 5000);
+  
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setActivity(`Sefa MERT ™ Tarafından Geliştirildi. | youtube.com/SefaMERT`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ${client.channels.cache.size} adet kanala, ${client.guilds.cache.size} sunucuya ve ${client.users.cache.size} kullanıcıya hizmet veriliyor!`);
+
+
 };

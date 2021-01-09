a6fd945ab5bbd357a23903cf95eb0672040371b4
Sefa MERT <64640906+sefa60@users.noreply.github.com>
2021-01-09 17:30:44 +0300

    Update botekle-onayla.js
    Sefa MERT

--- komutlar/botekle-onayla.js
+++ komutlar/botekle-onayla.js
@@ -3,7 +3,7 @@
 
   exports.run = function(client, message, args) {
 
-  if (!message.member.roles.has("BOT-KONTROL-ROL-İD")) return message.channel.send(`**Bu komutu kullanmnız için yetkiniz yetmiyor.**`);
+  if (!message.member.roles.has("BOT-KONTROL-ROL-İD")) return message.channel.send(`**:warning: ・ Bu komutu kullanmnız için yetkiniz yetmiyor.**`);
 
 	let sefamert = args[0]
   let sahip = args[1]
@@ -12,10 +12,10 @@
 	let atlanticode = "KANAL-İD" // bot durum log kanalı
   let botsahipkanal = "KANAL-İD" // bot sahiplerinin gösterileceği kanal
   
-	if (message.channel.id !== kanal) return message.channel.send(`**Başvuruda bulunulmuş bir botu yalnızca <#${kanal}> kanalında onaylayabilirsin.**`).then(msg => msg.delete(5000))
-  if (!sefamert) return message.channel.send(`**Botun idsini yazmalısın.**`).then(msg => msg.delete(5000))
-  if (!sahip) return message.channel.send(`**Botun sahibinin idsini yazmalısın.**.`).then(msg => msg.delete(5000))
-   if (!prefix) return message.channel.send(`**Botun prefixini yazmalısın.**.`).then(msg => msg.delete(5000))
+	if (message.channel.id !== kanal) return message.channel.send(`**ℹ ・ Başvuruda bulunulmuş bir botu yalnızca <#${kanal}> kanalında onaylayabilirsin.**`).then(msg => msg.delete(5000))
+  if (!sefamert) return message.channel.send(`**:exclamation: ・ Botun idsini yazmalısın.**`).then(msg => msg.delete(5000))
+  if (!sahip) return message.channel.send(`**:exclamation: ・ Botun sahibinin idsini yazmalısın.**.`).then(msg => msg.delete(5000))
+   if (!prefix) return message.channel.send(`**:exclamation: ・ Botun prefixini yazmalısın.**.`).then(msg => msg.delete(5000))
   message.delete()
   const embed = new Discord.RichEmbed()
   .setColor("RANDOM")
@@ -31,14 +31,14 @@
   .setFooter("Bu bot, Sefa MERT tarafından geliştirlmiştir.",client.user.avatarURL)
   client.channels.get(botsahipkanal).send(embed)
   
-  client.channels.get(atlanticode).send(`<@${sahip}> **adlı geliştiricinin; <@${sefamert}> adlı, __${sefamert}__ idli botu onaylandı.** \n <a:gul:794639948409339914> **Bizimle çalıştığınız için teşekkürler, <@${sahip}>!**\n <a:hyq:794637845255421953> __**Botu Onaylayan Yetkili :**__ ${message.author} - **${message.author.tag}**`)
-  message.channel.send(`**Başarıyla botu onayladınız.**`).then(msg => msg.delete(10000))
+  client.channels.get(atlanticode).send(`<@${sahip}> **adlı geliştiricinin; <@${sefamert}> adlı, __${sefamert}__ idli botu onaylandı.** \n **Bizimle çalıştığınız için teşekkürler, <@${sahip}>!**\n __**Botu Onaylayan Yetkili :**__ ${message.author} - **${message.author.tag}**`)
+  message.channel.send(`**:white_check_mark: ・ Başarıyla botu onayladınız.**`).then(msg => msg.delete(10000))
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['bot-onayla', 'onayla','onay','o'],
+  aliases: ['bot-onayla'],
   permLevel: 0
 };
 

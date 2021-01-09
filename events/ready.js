4fc4889c907d2e48a730d9619a00110f1f734d64
Sefa MERT <64640906+sefa60@users.noreply.github.com>
2021-01-10 01:21:33 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,30 +1,16 @@
-const chalk = require('chalk');
-const moment = require('moment');
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
-
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
 var prefix = ayarlar.prefix;
 
-module.exports = client => {
-var oyun = [
-        "SefaMERT ♥",
-        "youtube.com/SefaMERT"
-     
-    ];
+module.exports = async client => {
+  var atlanticode = ["youtube.com/SefaMERT"];
 
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setActivity(oyun[random], "youtube.com/SefaMERT" );
-      }, 2 * 5000);
-  
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  client.user.setActivity(`Sefa MERT ™ Tarafından Geliştirildi. | youtube.com/SefaMERT`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ${client.channels.cache.size} adet kanala, ${client.guilds.cache.size} sunucuya ve ${client.users.cache.size} kullanıcıya hizmet veriliyor!`);
+  setInterval(async () => {
+    var random = Math.floor(Math.random() * (atlanticode.length - 0 + 1) + 0);
+  }, 12000);
+  client.user.setStatus("dnd");
 
 
 };
